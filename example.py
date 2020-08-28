420c36b80bce0cb5ba527a923bcc147039cf42f9
TrevorWinstral <trevtdw@gmail.com>
2020-08-28 11:54:04 +0200

    First Commit

---
+++ example.py
@@ -0,0 +1,3 @@
+import tiny
+
+tiny.compress_image(uncompressed_image='home_header.jpg', compressed_image='out.jpg')
---
+++ tiny.py
@@ -0,0 +1,22 @@
+import sys
+import tinify
+
+def compress_image(uncompressed_image='input.jpg', compressed_image='compressed.jpg'):
+	try:	
+		if len(sys.argv) > 1:
+			uncompressed_image=sys.argv[1]
+			compressed_image=sys.argv[2]
+
+		with open('key.txt', 'r') as inp:
+			KEY = inp.read()
+			tinify.key = KEY
+
+		src = tinify.from_file(uncompressed_image)
+		src.to_file(compressed_image)
+		return True
+
+	except:
+		return False
+
+if __name__ == '__main__':
+	compress_image()
